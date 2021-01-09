08be66df2f0cc13da67099bef6c7860f329d0b7a
Stereowalker <stereowalker.mc@gmail.com>
2021-01-09 03:50:18 +0100

    Fixed hooks not behaving properly

--- src/main/java/com/stereowalker/unionlib/hook/UnionHooks.java
+++ src/main/java/com/stereowalker/unionlib/hook/UnionHooks.java
@@ -1,17 +1,13 @@
 package com.stereowalker.unionlib.hook;
 
-import java.util.Collection;
-
 import com.stereowalker.unionlib.events.ItemCraftedEvent;
 import com.stereowalker.unionlib.events.StructureAddedEvent;
 import com.stereowalker.unionlib.events.StructurePieceAddedEvent;
-import com.stereowalker.unionlib.events.TileIdRegisteredEvent;
 
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.CraftingInventory;
 import net.minecraft.inventory.container.CraftingResultSlot;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.ISeedReader;
 import net.minecraft.world.gen.WorldGenRegion;
 import net.minecraft.world.gen.feature.structure.StructurePiece;
@@ -20,11 +16,6 @@ import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.common.MinecraftForge;
 
 public class UnionHooks {
-
-	public static void fireTileIdRegistered(Collection<ResourceLocation> tileIds )
-	{
-		MinecraftForge.EVENT_BUS.post(new TileIdRegisteredEvent(tileIds));
-	}
 
 	public static void fireStructureAdded(StructureStart<?> structureStart, ServerWorld world)
 	{
